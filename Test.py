895e07fa5ae0d443236b7d12418b8863b8647ca0
JoeVenner <ylafrimi@gmail.com>
2020-08-05 23:51:10 +0100

    Add files via upload
    Test Cam and model

---
+++ Test.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+import time, pafy
+
+# OpenCV Facial Capture Test 
+url = 'https://www.youtube.com/watch?v=lQnLwUfwgyA'
+vpafy = pafy.new(url)
+play = vpafy.getbest(preftype="mp4")
+
+_cap = cv.VideoCapture(play.url)
+_cap.set(cv.CAP_PROP_FRAME_WIDTH, 512)
+_cap.set(cv.CAP_PROP_FRAME_HEIGHT, 512)
+_cap.set(cv.CAP_PROP_BUFFERSIZE, 1)
+time.sleep(0.5)
+
+facemark = cv.face.createFacemarkLBF()
+try:
+    # Download the trained model lbfmodel.yaml:
+    # https://github.com/kurnianggoro/GSOC2017/tree/master/data
+    # and update this path to the file:
+    facemark.loadModel("./data/lbfmodel.yaml")
+except cv.error:
+    print("Model not found")
+
+cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_alt.xml")
+if cascade.empty() :
+    print("cascade not found")
+    exit()
+
+print("Press ESC to stop")
+while True:
+    _, frame = _cap.read()
+    
+    faces = cascade.detectMultiScale(frame, 1.05,  6, cv.CASCADE_SCALE_IMAGE, (130, 130))
+    
+    #find biggest face, and only keep it
+    if(type(faces) is np.ndarray and faces.size > 0):
+        biggestFace = np.zeros(shape=(1,4))
+        for face in faces:
+            if face[2] > biggestFace[0][2]:
+                biggestFace[0] = face
+
+        # find landmarks
+        ok, landmarks = facemark.fit(frame, faces=biggestFace)
+        
+        # draw landmarks
+        for marks in landmarks:
+            for (x, y) in marks[0]:
+                cv.circle(frame, (x, y), 2, (0, 255, 255), -1)  
+        
+        # draw detected face
+        for (x,y,w,h) in faces:
+            cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 1)
+
+        for i,(x,y,w,h) in enumerate(faces):
+            cv.putText(frame, "Face #{}".format(i), (x - 10, y - 10),
+                            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+    cv.imshow("Image Landmarks", frame)
+    if(cv.waitKey(1) == 27):
+        exit()
+
